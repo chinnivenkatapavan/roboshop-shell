6eaa1617b85cb4c124ce8b48da9b01a5582f68b4
chinni venkatapavan <chinnivenkatapavan@gmil.com>
2025-01-05 14:15:38 +0530

    Adding DNS date in files

--- redis.sh
+++ redis.sh
@@ -4,7 +4,7 @@ dnf module enable redis:7 -y
 dnf install redis -y
 
 sed -i 's/127.0.0.1/0.0.0.0' /etc/redis/redis.conf
-sed -i 's/protected-mode yes/protected-mode no' /etc/redis/redis.conf
+sed -i '/protected-mode/ c protected-mode no' /etc/redis/redis.conf
 
 systemctl enable redis
 systemctl start redis
