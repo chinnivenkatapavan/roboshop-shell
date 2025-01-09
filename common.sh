272b9dfeb067e70c3fec403c99c9833fb0498cf7
chinni venkatapavan <chinnivenkatapavan@gmil.com>
2025-01-09 07:24:15 +0530

    Added New concept in my list

--- common.sh
+++ common.sh
@@ -1,7 +1,5 @@
 dir_path=$(pwd)
 
-dir_path=$(pwd)
-
 NodeJs(){
   dnf module disable nodejs -y
   dnf module enable nodejs:20 -y
@@ -23,11 +21,32 @@ UserAdd(){
   npm install
 }
 
-DirPath{
+DirPath(){
   cp ${dir_path}/${app_name}.service /etc/systemd/system/${app_name}.service
 
   systemctl daemon-reload
 
   systemctl enable ${app_name}
   systemctl restart ${app_name}
+}
+
+JAVA(){
+
+
+  dnf install maven -y
+
+  useradd roboshop
+
+  rm -rf /app
+  mkdir /app
+
+  curl -L -o /tmp/${app_name}.zip https://roboshop-artifacts.s3.amazonaws.com/${app_name}-v3.zip
+  cd /app
+  unzip /tmp/${app_name}.zip
+
+  cd /app
+  mvn clean package
+  mv target/${app_name}-1.0.jar ${app_name}.jar
+
+  DirPath
 }
--- shipping.sh
+++ shipping.sh
@@ -1,24 +1,4 @@
-cp shipping.service /etc/systemd/system/shipping.service
-
-dnf install maven -y
-
-useradd roboshop
-
-rm -rf /app
-mkdir /app
-
-curl -L -o /tmp/shipping.zip https://roboshop-artifacts.s3.amazonaws.com/shipping-v3.zip
-cd /app
-unzip /tmp/shipping.zip
-
-cd /app
-mvn clean package
-mv target/shipping-1.0.jar shipping.jar
-
-systemctl daemon-reload
-
-systemctl enable shipping
-systemctl start shipping
+app_name=shipping
 
 dnf install mysql -y
 
