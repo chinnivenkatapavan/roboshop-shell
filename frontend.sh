24b508b55c3f5e930d346774bee72b4621a00a0f
chinni venkatapavan <chinnivenkatapavan@gmil.com>
2025-01-11 18:23:41 +0530

    Added New concept in my list

--- frontend.sh
+++ frontend.sh
@@ -1,17 +1,20 @@
 app_name=nginx
 
-
+echo Showing module list
 dnf module list
 
-dnf module disable ${app_name} -y
-dnf module enable ${app_name}:1.24 -y
-dnf install ${app_name} -y
+echo install modules &>>log_file
+dnf module disable ${app_name} -y &>>log_file
+dnf module enable ${app_name}:1.24 -y &>>log_file
+dnf install ${app_name} -y &>>log_file
+Status_Print $?
+
 
 rm -rf /etc/${app_name}/${app_name}.conf
 cp ${app_name}.conf /etc/${app_name}/${app_name}.conf
 
-systemctl enable ${app_name}
-systemctl start ${app_name}
+systemctl enable ${app_name} &>>log_file
+systemctl start ${app_name} &>>log_file
 
 
 rm -rf /usr/share/${app_name}/html/*
@@ -23,4 +26,4 @@ curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v
 cd /usr/share/${app_name}/html
 unzip /tmp/frontend.zip
 
-systemctl restart ${app_name}
+systemctl restart ${app_name} &>>log_file
