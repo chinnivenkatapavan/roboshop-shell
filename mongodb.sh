b3b1d0d5af2fdc2369dc4e0ec34103fed5d8c731
chinni venkatapavan <chinnivenkatapavan@gmil.com>
2025-01-11 18:35:15 +0530

    Added New concept in my list

--- mongodb.sh
+++ mongodb.sh
@@ -1,7 +1,10 @@
+source ./common.sh
 
 cp mongo.repo /etc/yum.repos.d/mongo.repo
 
-dnf install mongodb-org -y
+echo Install mongoDB file in project
+dnf install mongodb-org -y &>>log_file
+Status_Print $?
 
 sed -i 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
 
